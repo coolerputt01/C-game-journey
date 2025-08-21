7d61e970dd75171f30ab850456e04d78f1284b1d
coolerputt01 <olumideadekolu9@gmail.com>
2025-08-21 13:58:29 +0100

    Made some progreds

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include <SFML/Graphics.hpp>
 #include <optional>
-
+#include <vector>
 int main() {
     // In SFML 3, VideoMode takes a Vector2u
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML in Termux VNC");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    std::vector<sf::CircleShape> triangles;
 
+sf::RenderWindow window(sf::VideoMode({1280, 900}), "SFML in Termux VNC");
+   float factor = 40.f;   
+   float height = 0.f;
+   for(int i = 0; i <= 3;i++){
+sf::CircleShape triangle(80.f,3);
+    triangle.setFillColor(sf::Color(56, 160, 75));
+    triangle.setPosition({40.f,height});
+    height += factor;
+    triangles.push_back(triangle);
+}
     while (window.isOpen()) {
         // pollEvent now returns std::optional<sf::Event>
         while (auto event = window.pollEvent()) {
@@ -15,8 +23,10 @@ int main() {
             }
         }
 
-        window.clear();
-        window.draw(shape);
+        window.clear(sf::Color(255,255,255));
+       for(auto &t : triangles){ 
+window.draw(t);
+}
         window.display();
     }
     return 0;
